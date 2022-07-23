833ad8293b60116cea88d99d008a362264eaf8ff
Andreas <andreas.kraftl@kraftl.at>
2022-07-23 10:50:20 +0200

    bugfix and root check

--- templates/scripts/vm_check.sh
+++ templates/scripts/vm_check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $EUID > 0 ]]
+then
+	echo "You must be root" >&2
+	exit 1
+fi
+
 for ip in {{ vms | map(attribute='network.0') | map(attribute='ip') | list | join(' ') }}
 do
 	sshpass -p {{ vm_root_password }} ssh \
@@ -8,7 +14,7 @@ do
 		-o UserKnownHostsFile=/dev/null \
 		-o LogLevel=ERROR \
 		root@${ip} \
-		hostnamectl | fgrep "Static hostname: " \
+		hostnamectl | fgrep "Static hostname: " | \
                 sed -Ee 's/^ *(.+)/UP \1/'
 done
 	
